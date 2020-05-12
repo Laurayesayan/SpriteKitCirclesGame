aa5966ed722d6fed3d9624d51c59f04108bb5c0b
laurayesayan <laurayesayan@gmail.com>
2020-05-12 23:39:28 +0300

    Added game scene where two circles moving around.

--- SpriteKitGame_2_16/GameScene.swift
+++ SpriteKitGame_2_16/GameScene.swift
@@ -10,80 +10,66 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var player: SKShapeNode! = nil
+    var enemy: SKShapeNode! = nil
+    var scoreLabel: SKLabelNode! = nil
+
+    var counter = 0
     
     override func didMove(to view: SKView) {
+        let radius = view.frame.width / 12
+        player = initializeNode(with: radius, color: #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1))
+        enemy = initializeNode(with: radius, color: #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1), position: CGPoint(x: 0, y: view.frame.height / 4))
+        scoreLabel = createLabelNode(text: "Score: 0", position: CGPoint(x: view.frame.width / 2, y: view.frame.height / 2))
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        self.addChild(player)
+        self.addChild(enemy)
+        self.addChild(scoreLabel)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        moveEnemy()
+        increaseEnemySize()
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        super.touchesEnded(touches, with: event)
+        
+        move(node: player, to: (touches.first?.location(in: self))!, completion: nil)
     }
     
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    func createLabelNode(text: String, position: CGPoint) -> SKLabelNode {
+        let labelNode = SKLabelNode(text: text)
+        labelNode.fontColor = #colorLiteral(red: 0.9686274529, green: 0.8910074234, blue: 0.03772118949, alpha: 1)
+        labelNode.position = CGPoint(x: position.x - labelNode.frame.width, y: position.y - labelNode.frame.height)
+        return labelNode
     }
     
+    func initializeNode(with radius: CGFloat, color: UIColor, position: CGPoint? = nil) -> SKShapeNode {
+        let node = SKShapeNode(circleOfRadius: radius)
+        node.fillColor = color
+        node.lineWidth = 0
+        node.position = position ?? CGPoint(x: 0, y: 0)
+        
+        return node
+    }
     
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+    func move(node: SKNode, to position: CGPoint, completion: (() -> Void)? = nil) {
+        let move = SKAction.move(to: position, duration: 1.0)
+        node.run(move, completion: completion ?? {})
+    }
+    
+    func moveEnemy() {
+        move(node: enemy, to: player.position, completion: moveEnemy)
+        counter += 1
+        scoreLabel.text = "Score: \(counter)"
+        
+    }
+    
+    func increaseEnemySize() {
+        enemy.xScale += 0.1
+        enemy.yScale += 0.1
+        let waitAction = SKAction.wait(forDuration: 5.0)
+        counter += 1
+        scoreLabel.text = "Score: \(counter)"
+        enemy.run(waitAction, completion: increaseEnemySize)
     }
 }
